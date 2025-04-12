42ad1f8cf4df31f747695c373f7437cb18885a2b
PavelBlessYou51 <82117106+PavelBlessYou51@users.noreply.github.com>
2025-04-12 19:44:14 +0300

    first leson

---
+++ src/test/java/FirstLessonTests.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstLessonTests {
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void start() {
+        driver.get("https://www.google.ru/");
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        driver.quit();
+    }
+}
