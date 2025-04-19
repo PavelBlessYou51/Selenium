2b3187ca599ba28117d362668fd01fc77fcc5c46
PavelBlessYou51 <82117106+PavelBlessYou51@users.noreply.github.com>
2025-04-19 18:56:06 +0300

    1

--- src/test/java/FirstLessonTests.java
+++ src/test/java/FirstLessonTests.java
@@ -1,8 +1,10 @@
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class FirstLessonTests {
     private static WebDriver driver;
@@ -10,11 +12,14 @@ public class FirstLessonTests {
     @BeforeAll
     public static void setUp() {
         driver = new ChromeDriver();
+        DesiredCapabilities caps = new DesiredCapabilities();
+        System.out.println(((HasCapabilities)driver).getCapabilities());
     }
 
     @Test
     public void start() {
-        driver.get("http://localhost/litecart/admin/");
+        //driver.get("http://localhost/litecart/admin/");
+        System.out.println("!!!");
     }
 
     @AfterAll
